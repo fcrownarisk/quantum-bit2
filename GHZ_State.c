98b899d731bdee75d42a9b6dcafe3c8e347a6605
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-10-23 23:30:03 +0800

    Add files via upload

---
+++ GHZ_State.c
@@ -0,0 +1,7 @@
+int bit(){
+    const fai = "<000| + |111>";
+    "inline" ;
+    const State = "<0000| - |1111>";
+    extern
+    const anti_State ; "<1111| - |0000>";
+}
---
+++ fai.c
@@ -0,0 +1,10 @@
+void fai(){
+    char gamma , theta;
+    const fai1 = "exp(sqrt(-1)*gamma)*cos(theta/2)*|0> + exp(sqrt(-1)*sin(theta/2)*|1>";
+    const fai2 = "cos(theta/2)*|0> + sin(theta/2)*|1>";
+    const fai3 = "1/sqrt(2)*[|0>+|1>]";
+}
+double Bell_Base(){
+    const fai4 = "1/sqrt(2)*[|00>+-|11>]";
+    const fai5 = "1/sqrt(2)*[|01>+-|01>]";
+}
